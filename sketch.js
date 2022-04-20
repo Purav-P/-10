1eaa669420f62aca38805ecd1dba4aea3438df6d
Purav-P <100454034+Purav-P@users.noreply.github.com>
2022-04-20 18:25:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("hello");
+console.log("my  name is Purav")
+console.log("This is my first project")
+console.log("I go to school at Stone Elementary")
+console.log("Once upon a time, there was a little princess wanting to see the world, but her parents wouldn't let her even leave the castle. She was determined to go out and see the world, so she started to steal some of the guards tools to help her on her journey. by the time she had gotten enough materials, she wen to sneak out of the castle, but her parents were standing there waiting for her. She was confuzzled. Her parents knew what she was doing and wanted to say goodbye one last time. After that, the little determined princess left the castle and soon explored the whole world and honing her skills as a swordswoman. The End.")
